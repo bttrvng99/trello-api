0e3fad313025645858c8da7f84b1b12186f74e60
Trung <bttrung3399@gmail.com>
2024-06-04 11:08:10 +0700

    Add environment variables and initialize connection to cloud DB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,40 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+// Tạo 1 đối tượng instance dành cho database (vì chưa connect)
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  // Lưu ý: serverApi có từ MongoDb 5.0.0 trở lên có thể không tương thích
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Kết nối tới Database
+export const CONNECT_DB = async () => {
+  //Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của clientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy database theo name và gán ngược nó lại với biến trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+// Đóng kết nối tới database khi cần
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+// Function GET_DB (không có async) có nhiệm vụ export ra Trello Database Instance sau khi đã connect
+// thành công tới MongoDB để được sử dụng ở nhiều nơi khác nhau trong code.
+// Lưu ý phải đảm bảo chỉ luôn gọi cái getDB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to database first!')
+  return trelloDatabaseInstance
+}
