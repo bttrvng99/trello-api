17565d78fbfdd67a6f707429828e84ee98079b33
Trung <bttrung3399@gmail.com>
2024-06-07 10:01:23 +0700

    Add API to get full details of board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,7 @@
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -26,6 +28,18 @@ const createNew = async (reqBody) => {
   } catch (error) { throw error }
 }
 
+const getDetails = async (boardId) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+
+    return board
+  } catch (error) { throw error }
+}
+
 export const boardService = {
-  createNew
+  createNew, getDetails
 }
